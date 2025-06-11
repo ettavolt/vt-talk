e75549b14d3a54f9d3e8a8c3b619213ccc157c75
ettavolt <ettavolt@gmail.com>
2025-06-11 11:33:43 -0400

    RPS reporting in EchoServer.

--- jvm/src/main/java/org/example/EchoServer.java
+++ jvm/src/main/java/org/example/EchoServer.java
@@ -7,27 +7,18 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.time.Clock;
+import java.time.*;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EchoServer {
+
 	public static void main(String[] args) throws IOException {
-		var server = HttpServer.create(
-			new InetSocketAddress(InetAddress.getLoopbackAddress(), 8080),
-			0,
-			"/",
-			EchoServer::doHandle
-		);
-		server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-		server.start();
-		System.out.println("Started EchoServer.");
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			server.stop(0);
-			System.out.println("Stopped EchoServer.");
-		}, "EchoServer-Shutdown"));
+		new EchoServer().start();
 	}
 
-	private static void doHandle(HttpExchange exchange) throws IOException {
+	private void doHandle(HttpExchange exchange) throws IOException {
 		var buf = new ByteArrayOutputStream();
 		var writer = new PrintWriter(buf, true, StandardCharsets.UTF_8);
 		writer.append("\nmethod: ").append(exchange.getRequestMethod()).append("\n");
@@ -48,6 +39,55 @@ public class EchoServer {
 		try (var responseBody = exchange.getResponseBody()) {
 			buf.writeTo(responseBody);
 		}
-		System.out.println(Clock.systemUTC().millis());
+		counter.incrementAndGet();
+	}
+
+	private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+	private final AtomicInteger counter = new AtomicInteger();
+	private final HttpServer server = HttpServer.create(
+		new InetSocketAddress(InetAddress.getLoopbackAddress(), 8080),
+		0,
+		"/",
+		this::doHandle
+	);
+
+	public EchoServer() throws IOException {
+		server.setExecutor(executor);
+	}
+
+	public void start() {
+		executor.submit(this::reportRps);
+		server.start();
+		System.out.println("Started " + this);
+		//Note: virtual threads cannot be "created, but not started".
+		Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown, this + "-shutdown"));
+	}
+
+	private final Duration aSecond = Duration.ofSeconds(1);
+	private void reportRps() {
+		try {
+			var prevFellAsleep = 0L;
+			var fellAsleep = System.nanoTime();
+			//See the comment before "catch".
+			//noinspection InfiniteLoopStatement
+			do {
+				prevFellAsleep = fellAsleep;
+				var rps = counter.getAndSet(0);
+				if (rps > 0) {
+					System.out.println(ZonedDateTime.now() + " " + rps);
+				}
+				fellAsleep = System.nanoTime();
+				Thread.sleep(aSecond.minus(Duration.ofNanos(fellAsleep - prevFellAsleep)));
+			} while (true);
+			//InterruptedException: thrown if the thread was interrupted before or during sleep activity.
+		} catch (InterruptedException ignored) {
+			//Time to go off.
+		}
+	}
+
+	private void shutdown() {
+		server.stop(0);
+		executor.shutdownNow();
+		System.out.println("Stopped " + this);
 	}
 }
